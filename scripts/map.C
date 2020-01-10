3112d560057a0f2606f592212b0a0b74e0011f70
jtingey <j.tingey.16@ucl.ac.uk>
2020-01-10 16:27:39 +0000

    feat: change mapping macro to comply with WCSimMapper not requiring event category anymore

--- scripts/map.C
+++ scripts/map.C
@@ -3,15 +3,15 @@ void load_libs()
 	gSystem->Load("libGeom");
 	gSystem->Load("libEve");
 	gSystem->Load("libMinuit");
-	TString libWCSimRoot = TString::Format("%s%s", gSystem->Getenv("WCSIMHOME"), "/libWCSim.so");
-	TString libWCSimAnalysis = TString::Format("%s%s", gSystem->Getenv("WCSIMANAHOME"), "/libWCSimAnalysis.so");
+	TString libWCSimRoot = TString::Format("%s%s", gSystem->Getenv("WCSIMHOME"), "/lib/libWCSim.so");
+	TString libWCSimAnalysis = TString::Format("%s%s", gSystem->Getenv("WCSIMANAHOME"), "/lib/libWCSimAnalysis.so");
 	gSystem->Load(libWCSimRoot.Data());
 	gSystem->Load(libWCSimAnalysis.Data());	
 }
 
-void map(const char * in_file = "", const char * out_file = "", int max_events=1000, int category=0, int pdg=11) 
+void map(const char * in_file = "", const char * out_file = "", int max_events=1000, int pdg=11) 
 {
 	load_libs();
-	WCSimMapper mapper = WCSimMapper(in_file, out_file, max_events, category, pdg);
+	WCSimMapper mapper = WCSimMapper(in_file, out_file, max_events, pdg);
 	mapper.run();
 }
