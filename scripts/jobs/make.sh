4ef1e31c6d1fa24456eeb565f1ddcfde5339646e
jtingey <j.tingey.16@ucl.ac.uk>
2020-04-09 13:25:24 +0100

    feat: make.sh script now creates the production directories

--- scripts/jobs/make.sh
+++ scripts/jobs/make.sh
@@ -2,6 +2,48 @@
 
 PRODDIR=/unix/chips/prod
 
+mkdir $PRODDIR
+
+mkdir $PRODDIR/beam_all/
+mkdir $PRODDIR/beam_nuel/
+mkdir $PRODDIR/beam_numu/
+mkdir $PRODDIR/cosmic_all/
+
+mkdir $PRODDIR/beam_all/nuel/
+mkdir $PRODDIR/beam_all/anuel/
+mkdir $PRODDIR/beam_all/numu/
+mkdir $PRODDIR/beam_all/anumu/
+
+mkdir $PRODDIR/beam_nuel/cccoh/
+mkdir $PRODDIR/beam_nuel/nccoh/
+mkdir $PRODDIR/beam_nuel/ccdis/
+mkdir $PRODDIR/beam_nuel/ncdis/
+mkdir $PRODDIR/beam_nuel/ccqel/
+mkdir $PRODDIR/beam_nuel/ncqel/
+mkdir $PRODDIR/beam_nuel/ccres/
+mkdir $PRODDIR/beam_nuel/ncres/
+mkdir $PRODDIR/beam_nuel/charmccdis/
+mkdir $PRODDIR/beam_nuel/charmccqel/
+mkdir $PRODDIR/beam_nuel/nueelastic/
+mkdir $PRODDIR/beam_nuel/imd/
+mkdir $PRODDIR/beam_nuel/ccmec/
+mkdir $PRODDIR/beam_nuel/ncmec/
+
+mkdir $PRODDIR/beam_numu/cccoh/
+mkdir $PRODDIR/beam_numu/nccoh/
+mkdir $PRODDIR/beam_numu/ccdis/
+mkdir $PRODDIR/beam_numu/ncdis/
+mkdir $PRODDIR/beam_numu/ccqel/
+mkdir $PRODDIR/beam_numu/ncqel/
+mkdir $PRODDIR/beam_numu/ccres/
+mkdir $PRODDIR/beam_numu/ncres/
+mkdir $PRODDIR/beam_numu/charmccdis/
+mkdir $PRODDIR/beam_numu/charmccqel/
+mkdir $PRODDIR/beam_numu/nueelastic/
+mkdir $PRODDIR/beam_numu/imd/
+mkdir $PRODDIR/beam_numu/ccmec/
+mkdir $PRODDIR/beam_numu/ncmec/
+
 python $CHIPSENV/scripts/run.py $PRODDIR/beam_all/nuel/ --make
 python $CHIPSENV/scripts/run.py $PRODDIR/beam_all/anuel/ --make
 python $CHIPSENV/scripts/run.py $PRODDIR/beam_all/numu/ --make
