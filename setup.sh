9f7bdbc26bd34dce1a2f9a68c0c6d11471c918e2
jtingey <j.tingey.16@ucl.ac.uk>
2020-05-14 10:26:59 +0100

    tidy: .gitignore files and setup script logging

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ CURRENTDIR=$(pwd)
 
 # Ensure that the script has been sourced rather than just executed
 if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
-    echo "${C_RED}ERROR${C_RESET}:   Please use 'source' to execute setup.sh"
+    echo "${C_RED}ERROR:${C_RESET}   Please use 'source' to execute setup.sh"
     return
 fi
 
@@ -21,26 +21,26 @@ export SINGULARITY_BIND="$CHIPSENV:/opt/chips-env,$GEANT4DATA:/opt/data/geant4"
 
 # Check if singularity is installed
 if ! [ -x "$(command -v singularity)" ]; then
-    echo "${C_RED}ERROR${C_RESET}:   Singularity is not installed"
+    echo "${C_RED}ERROR:${C_RESET}   Singularity is not installed"
     return
 fi
 
 # Check the chips-env.sif image is present if not download it
 if ! [ -f "$CHIPSENV/env/chips-env.sif" ]; then
-    echo "${C_BLUE}INFO${C_RESET}:    Downloading singularity image"
+    echo "${C_BLUE}INFO:${C_RESET}    Downloading singularity image"
     singularity pull $CHIPSENV/env/chips-env.sif library://joshtingey/default/chips-env:latest
 fi
 
 singularity_run() {
     if [ $# -eq 0 ]; then
-        echo "${C_BLUE}INFO${C_RESET}:    Starting shell"
+        echo "${C_BLUE}INFO:${C_RESET}    Starting shell"
         singularity shell $CHIPSENV/env/chips-env.sif
     else
         if [ $1 == "exec" ]; then
-            echo "${C_BLUE}INFO${C_RESET}:    Executing command (${@:2}) in container"
+            echo "${C_BLUE}INFO:${C_RESET}    Executing command (${@:2}) in container"
             singularity 'exec' $CHIPSENV/env/chips-env.sif ${@:2}
         else
-            echo "${C_BLUE}INFO${C_RESET}:    Running application ($1) with args (${@:2})"
+            echo "${C_BLUE}INFO:${C_RESET}    Running application ($1) with args (${@:2})"
             singularity run --app $1 $CHIPSENV/env/chips-env.sif ${@:2}
         fi
     fi
@@ -51,4 +51,4 @@ alias chips=singularity_run
 # Go back to the user directory
 cd $CURRENTDIR
 
-echo "${C_BLUE}INFO${C_RESET}:    Setup done"
+echo "${C_BLUE}INFO:${C_RESET}    Setup done"
